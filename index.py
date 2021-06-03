28ebddf9807cdd25a39953f35d48a813ef3955a8
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2021-06-03 17:25:40 +0530

    Extracting the COVID-19 dataset

--- index.py
+++ index.py
@@ -2,3 +2,8 @@
 import pycountry
 import plotly.express as px
 import pandas as pd
+
+#Exrtracting the data from the GitHub COVID-19 Data
+database = r'https://raw.githubusercontent.com/datasets/covid-19/master/data/countries-aggregated.csv'
+data_1 = pd.read_csv(database)
+print(data_1)
